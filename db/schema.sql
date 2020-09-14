b2f7f6d4f86de7d58000fe9f710a6e93ff3b1cb6
Jesus DeLintt <jesus.delintt@gmail.com>
2020-09-13 21:17:26 -0700

    created a config folder and connection.js file

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,14 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
+
+CREATE TABLE burers
+(
+    id INT
+    AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR
+    (30) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY
+    (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Swiss', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Jalapeno Jack', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Chicken', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Mushroom', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bacon and Egg', false);
