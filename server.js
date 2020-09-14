933b35d800cae408c530bd4be62a758c5fa12539
Jesus DeLintt <jesus.delintt@gmail.com>
2020-09-14 16:18:28 -0700

    requird on server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 const express = require("express");
+var bodyParser = require("body-parser");
+var express = require("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+app.listen(PORT, function () {
+  console.log("Sever listening on PORT" + PORT);
+});
