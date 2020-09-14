05c652edd7a3645c87e6a969ff5f1f4de050cd81
Jesus DeLintt <jesus.delintt@gmail.com>
2020-09-14 16:30:27 -0700

    use route

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 var bodyParser = require("body-parser");
-var express = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 var app = express();
 var PORT = process.env.PORT || 8000;
+
+app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
 
 app.listen(PORT, function () {
   console.log("Sever listening on PORT" + PORT);
