33e89fa8e7b9258594242b889bf14c96637e65d6
Jesus DeLintt <jesus.delintt@gmail.com>
2020-09-17 20:20:15 -0700

    changed to connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
 if (process.env.JAWSDB_URL) {
   // Database is JawsDB on Heroku
   connection = mysql.createConnection(process.env.JAWSDB_URL);
