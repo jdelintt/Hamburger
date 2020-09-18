21a461f4b6b90a918d37ebba9e290f9e79f91b3c
Jesus DeLintt <jesus.delintt@gmail.com>
2020-09-17 19:47:05 -0700

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "d1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "mypass",
-  database: "burgers_db",
+  user: "umfspx2r92u2qkz4",
+  password: "a7blh59xlfey8m59",
+  database: "yj0f7aypky5rwgua",
 });
 
 // Make connection.
